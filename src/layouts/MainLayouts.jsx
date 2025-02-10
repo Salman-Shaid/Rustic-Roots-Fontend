fe9cc1091f4944afa1e75300b80b574c96de1075
Salman-Shaid <salmanshaid9@gmail.com>
2025-02-11 03:39:03 +0600

    add  some elements

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -11,12 +11,14 @@ const MainLayouts = () => {
 
     return (
         <div className={`min-h-screen ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-green-50 text-gray-900'}`}>
+            {/* navbar  */}
             <Navbar></Navbar>
 
             <main className="min-h-screen">
                 <Outlet></Outlet>
             </main>
 
+            {/* footer */}
             <Footer></Footer>
         </div>
     );
